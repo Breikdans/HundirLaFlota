ac34df39c28ee29bb2c9b797d9e9def7aaa1f42c
Breikdans <Breikdans@users.noreply.github.com>
2015-11-19 09:37:01 +0100

    Actualizado Grid.cpp
    corregido modo de acceso al elemento de _tbl_Grid

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -7,18 +7,18 @@
 usint16& Grid::operator() (usint16 F, usint16 C)
 {
 	//	LANZAMOS EXCEPCION
-	if (F == 0 || C == 0 || F > MAX_ROWS_GRID || C > MAX_COLS_GRID)
+	if (F == 0 || C == 0 || F >= MAX_ROWS_GRID || C >= MAX_COLS_GRID)
 		throw std::out_of_range("Indice fuera de limites");
 
-	return _tbl_Grid[MAX_COLS_GRID*F + C];
+	return _tbl_Grid[F][C];
 }
 
 /* para acceder a una posicion y DEVOLVER su valor */
 usint16 CMatriz::operator() (usint16 F, usint16 C) const
 {
 	//	LANZAMOS EXCEPCION
-	if (F == 0 || C == 0 || F > MAX_ROWS_GRID || C > MAX_COLS_GRID)
+	if (F == 0 || C == 0 || F >= MAX_ROWS_GRID || C >= MAX_COLS_GRID)
 		throw std::out_of_range("Indice fuera de limites");
 
-	return _tbl_Grid[MAX_COLS_GRID*F + C];
+	return _tbl_Grid[F][C];
 }
